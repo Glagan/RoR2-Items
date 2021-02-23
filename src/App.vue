229e35b1b3504a3d435589b2381acd79c8d6c9b5
Glagan <Glagan@users.noreply.github.com>
2021-02-23 15:35:56 +0100

    Enable Clear button Avoid form submit Make search bar sticky

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,26 @@
 				<unicon name="github" width="48" height="48" fill="white"></unicon>
 			</a>
 		</h1>
-		<div class="w-full mb-4 overflow-hidden bg-green-50 text-green-600 rounded-md shadow-md">
+		<div class="w-full mb-1 overflow-hidden bg-green-50 text-green-600 rounded-md shadow-md">
 			<div class="p-4 flex items-center border-l-4 border-green-600 tracking-normal">
 				<unicon class="mr-2" name="check-circle" width="28" height="28" fill="#059669"></unicon>
 				Up to date with version <b>1.0.1.1</b> (September 1st 2020).
 			</div>
 		</div>
-		<form class="flex flex-nowrap mb-4">
-			<button class="rounded-l-md bg-red-600 text-white border-2 border-red-700 py-1 px-2">Clear</button>
+		<form class="flex flex-nowrap sticky top-0 py-3 bg-gray-700 shadow-b" @submit.prevent="">
+			<button
+				class="rounded-l-md bg-red-600 text-white border-2 border-red-700 py-1 px-2"
+				@click.prevent="clearFilter"
+			>
+				Clear
+			</button>
 			<input
 				class="flex-grow text-black placeholder-gray-500 border border-gray-200 rounded-r-md p-2 focus:border-light-blue-500 focus:ring-2 focus:ring-light-blue-500"
 				v-model="filter"
+				@keydown.enter.prevent=""
 			/>
 		</form>
-		<div class="body flex flex-row flex-wrap items-start justify-around">
+		<div class="body flex flex-row flex-wrap items-start justify-around mt-1">
 			<Item v-for="(item, index) in items" :key="index" :item="item" />
 		</div>
 	</div>
@@ -35,6 +41,11 @@ export default {
 	store,
 	created() {
 		this.$store.dispatch('load');
+	},
+	methods: {
+		clearFilter() {
+			this.filter = '';
+		},
 	},
 	computed: {
 		items() {
