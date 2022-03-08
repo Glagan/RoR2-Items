9b4a0191623e671a1b910c6031b78b9faadaabf2
Glagan <Glagan@users.noreply.github.com>
2022-03-08 13:12:23 +0100

    Update update message

--- src/components/UpdateAlert.vue
+++ src/components/UpdateAlert.vue
@@ -19,9 +19,20 @@
 			</div>
 		</div>
 	</div>
-	<div class="alert w-full mb-1 overflow-hidden bg-yellow-50 text-yellow-600 rounded-md shadow-md" v-else>
-		<div class="p-4 flex flex-row flex-nowrap items-center border-l-4 border-yellow-600 tracking-normal">
-			<XCircleIcon class="w-9 h-9 mr-2 text-yellow-600" />
+	<div
+		class="alert w-full mb-1 overflow-hidden rounded-md shadow-md"
+		:class="updating ? 'bg-blue-50 text-blue-600' : 'bg-yellow-50 text-yellow-600'"
+		v-else
+	>
+		<div
+			class="p-4 flex flex-row flex-nowrap items-center border-l-4 tracking-normal"
+			:class="updating ? 'border-blue-600' : 'border-yellow-600'"
+		>
+			<component
+				:is="updating ? 'RefreshIcon' : 'XCircleIcon'"
+				class="w-9 h-9 mr-2"
+				:class="updating ? 'text-blue-600' : 'text-yellow-600'"
+			/>
 			<div>
 				<p>
 					Outdated with version <b>{{ newVersion }}</b
@@ -37,17 +48,18 @@
 						<SteamIcon /> New patch <ExternalLinkIcon class="w-4 h-4 ml-1 text-blue-600" />
 					</a>
 				</p>
+				<p class="mt-4" v-if="updating">{{ updating }}</p>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { CheckCircleIcon, ExternalLinkIcon, XCircleIcon } from '@heroicons/vue/outline';
+import { CheckCircleIcon, ExternalLinkIcon, XCircleIcon, RefreshIcon } from '@heroicons/vue/outline';
 import SteamIcon from './SteamIcon.vue';
 
 export default {
-	components: { CheckCircleIcon, ExternalLinkIcon, XCircleIcon, SteamIcon },
+	components: { CheckCircleIcon, ExternalLinkIcon, XCircleIcon, RefreshIcon, SteamIcon },
 	data() {
 		return {
 			version: '1.1.1.2',
@@ -55,6 +67,8 @@ export default {
 			updateLink: 'https://store.steampowered.com/news/app/632360/view/3100140557216075596',
 			newVersion: '1.2.1.0',
 			newUpdateLink: 'https://store.steampowered.com/news/app/632360/view/3114806614742495252',
+			updating:
+				'All common, uncommon, rare, unique items and equipments are up to date, only the new corrupted and lunar items are missing.',
 		} as {
 			version: string;
 			date: string;
