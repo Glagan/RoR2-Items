4a865c48743851b9be9e64f48961e894455e98d6
Glagan <Glagan@users.noreply.github.com>
2024-07-03 09:51:28 +0200

    Add sentry and plausible

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,13 @@ import { createApp } from 'vue';
 import LazyLoadDirective from './directives/LazyLoadDirective';
 import App from './App.vue';
 import './index.css';
+import * as Sentry from '@sentry/vue';
 
-createApp(App).directive('lazyload', LazyLoadDirective).mount('#app');
+const app = createApp(App).directive('lazyload', LazyLoadDirective);
+
+Sentry.init({
+	app,
+	dsn: 'https://0949f913e2cf4b8e99365cf1e993c896@glitchtip.nikurasu.org/3',
+});
+
+app.mount('#app');
